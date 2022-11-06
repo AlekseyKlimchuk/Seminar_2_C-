3f5bb6f3fd4fb82e9a5a1ed22e7a66572f7daa2d
Aleksey <Aleksey.A.Klimchuk@gmail.com>
2022-11-06 20:44:00 +0300

    Add Example_13

---
+++ Example_13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет");
+Console.WriteLine("Введите  число: ");
+int numA = int.Parse(Console.ReadLine());
+
+void DefThirdDigit(int numA)
+{
+    int FirstDigit = numA % 100;
+    int SecondDigit = (numA - FirstDigit * 100) / 10;
+    int ThirdDigit = numA % 10;
+
+
+    if (numA < 100)
+    {
+        Console.WriteLine("Третей цифры нет");
+    }
+    else
+    {
+        Console.WriteLine("Третье число: " + ThirdDigit);
+    }
+}
+DefThirdDigit(numA);
