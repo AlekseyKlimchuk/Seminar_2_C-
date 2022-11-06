059bc855759ab2ade2ff338c04b6738a5ed3e257
Aleksey <Aleksey.A.Klimchuk@gmail.com>
2022-11-06 20:15:41 +0300

    Add Example_10

---
+++ Example_10/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа");
+Console.WriteLine("Введите трехзначное число: ");
+int numA = int.Parse(Console.ReadLine());
+
+void res (int NumA)
+{
+    int FirstDigit = numA % 100;
+    int SecondDigit = FirstDigit / 10;
+    Console.WriteLine(SecondDigit);
+}
+res (numA);
